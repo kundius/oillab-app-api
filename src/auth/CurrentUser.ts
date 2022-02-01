3737e26c0434e65fb5fb5a9563bfe35e21cdeb15
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-02-01 19:28:19 +0300

    исправление http авторизации

--- src/auth/CurrentUser.ts
+++ src/auth/CurrentUser.ts
@@ -3,7 +3,13 @@ import { GqlExecutionContext } from '@nestjs/graphql'
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
-    const ctx = GqlExecutionContext.create(context)
-    return ctx.getContext().req.user
+    const contextType = context.getType<'graphql' | 'http'>()
+    if (contextType === 'graphql') {
+      const ctx = GqlExecutionContext.create(context)
+      return ctx.getContext().req.user
+    }
+    if (contextType === 'http') {
+      return context.getArgByIndex(0).user
+    }
   }
 )
