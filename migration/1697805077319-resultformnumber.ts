9654de5aaed1596b94007208b466d3f045c0279e
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-10-20 17:11:00 +0300

    1

--- migration/1697805077319-resultformnumber.ts
+++ migration/1697805077319-resultformnumber.ts
@@ -4,11 +4,11 @@ export class resultformnumber1697805077319 implements MigrationInterface {
     name = 'resultformnumber1697805077319'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`oillab\`.\`result\` CHANGE \`number\` \`formNumber\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`result\` CHANGE \`number\` \`formNumber\` varchar(255) NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`oillab\`.\`result\` CHANGE \`formNumber\` \`number\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`result\` CHANGE \`formNumber\` \`number\` varchar(255) NOT NULL`);
     }
 
 }
